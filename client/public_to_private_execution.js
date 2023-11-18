a3eba59c41e4466a8fd7c46c7b9fed7530085df1
jordan <jordan@bitola.com>
2023-11-18 14:09:24 +0300

    Prof generates and verifies

--- client/public_to_private_execution.js
+++ client/public_to_private_execution.js
@@ -8,7 +8,8 @@ const publicKey2 = fs.readFileSync('./keys/account2.pub').toString()
 
 const inputValue = 2;
 // Convert to hex string
-const payload = ethers.hexlify(new Uint8Array([inputValue])).slice(2)
+//const payload = ethers.hexlify(new Uint8Array([inputValue])).slice(2)
+const payload = new Uint8Array([inputValue])
 console.log('Payload:', payload, typeof payload, payload.length)
 
 const s = sarma.construct(privateKey1, publicKey2, payload)
@@ -16,8 +17,9 @@ console.log('Sarma constructed:', s, typeof s, s.length)
 
 // Verify the sarma
 const p = sarma.verify(s, privateKey1)
-console.log('Sarma payload:', p, typeof p, p.length)
-console.log('Payload verified:', payload === p)
+// console.log('Sarma payload:', p, typeof p, p.length)
+// console.log('Payload:', payload, typeof payload, payload.length)
+console.log('Payload verified:', JSON.stringify(payload) === JSON.stringify(p))
 
 // Get the public key 1 and serialized it to a file
 const signingKey1 = new ethers.SigningKey(privateKey1)
@@ -25,7 +27,16 @@ const publicKey1 = signingKey1.publicKey; // This is a hex string
 // console.log('publicKey1', publicKey1, typeof publicKey1, publicKey1.length)
 
 const payloadBytes = ethers.toUtf8Bytes(s.slice(130))
-const payloadDigest = ethers.keccak256(payloadBytes)
+console.log('payloadBytes', payloadBytes, typeof payloadBytes, payloadBytes.length)
+const decoder = new TextDecoder('utf-8');
+const hexString = decoder.decode(payloadBytes);
+const decimalArray = [parseInt(hexString, 16)];
+console.log('decimalArray', decimalArray, typeof decimalArray, decimalArray.length)
+const uint8Array = new Uint8Array(decimalArray);
+const payloadDigest = ethers.keccak256(uint8Array)
+console.log('payloadDigest', payloadDigest, typeof payloadDigest, payloadDigest.length)
+
+// const payloadDigest = ethers.keccak256(payloadBytes)
 // console.log('payloadDigest', payloadDigest, typeof payloadDigest, payloadDigest.length)
 
 // Input parameters to the prover
--- client/sarma.js
+++ client/sarma.js
@@ -1,11 +1,23 @@
 const arblen_elgamal = require('./arblen_elgamal')
 const ethers = require('ethers')
 
+function toHexString(byteArray) {
+    result = ''
+    for (let i = 0; i < byteArray.length; i++) {
+        result += ('0' + byteArray[i].toString(16)).slice(-2)
+    }
+    return result;
+}
+
 function construct(privkey, recepient_pubkey, payload) {
+    console.log('Payload:', payload, typeof payload)
     // Sign the payload
     const signingKey = new ethers.SigningKey(privkey) // privkey us a hex string
-    const payloadBytes = ethers.toUtf8Bytes(payload)
-    const digest = ethers.keccak256(payloadBytes)
+    // const payloadBytes = ethers.toUtf8Bytes(payload)
+    // const digest = ethers.keccak256(payloadBytes)
+    // console.log('payload', payload, typeof payload, payload.length)
+    const digest = ethers.keccak256(payload)
+    console.log('digest', digest, typeof digest, digest.length)
     const signature = signingKey.sign(digest)
     
     // Verify the signature
@@ -14,19 +26,22 @@ function construct(privkey, recepient_pubkey, payload) {
         throw new Error("Signature mismatch")
     }
     // console.log("publicKey", signingKey.publicKey, typeof signingKey.publicKey, signingKey.publicKey.length)
-    // console.log("signature", JSON.stringify(signature))
+    console.log("signature", JSON.stringify(signature))
     const signatureHexString = signature.r.slice(2) + signature.s.slice(2) + signature.v.toString(16)
     // console.log("signatureHexString", signatureHexString, typeof signatureHexString, signatureHexString.length)
-    const sarma = arblen_elgamal.encrypt(recepient_pubkey, signatureHexString + payload)
-    // console.log('Sarma constructed:', sarma, typeof sarma, sarma.length)
+    const sarma = arblen_elgamal.encrypt(recepient_pubkey, signatureHexString + toHexString(payload))
+    console.log('Sarma constructed:', sarma, typeof sarma, sarma.length)
     return sarma
 }
 
 function verify(sarma, privkey) {
     const decryptedSarma = arblen_elgamal.decrypt(privkey, sarma)
     const signature = decryptedSarma.slice(0, 130)
-    const payloadBytes = ethers.toUtf8Bytes(decryptedSarma.slice(130))
+    console.log('sarma.slice(130)', sarma.slice(130), typeof sarma.slice(130))
+    const payloadBytes = ethers.getBytes('0x' + decryptedSarma.slice(130))
+    console.log('payloadBytes', payloadBytes, typeof payloadBytes, payloadBytes.length)
     const digest = ethers.keccak256(payloadBytes)
+    console.log('digest on verify', digest, typeof digest, digest.length)
     const signatureObject = {
         r: '0x' + signature.slice(0, 64),
         s: '0x' + signature.slice(64, 128),
@@ -40,7 +55,8 @@ function verify(sarma, privkey) {
     if (recoveredPublicKey !== signingKey.publicKey) {
         throw new Error("Signature mismatch")
     }
-    return decryptedSarma.slice(130)
+    //return decryptedSarma.slice(130)
+    return payloadBytes
 }
 
 module.exports = {
