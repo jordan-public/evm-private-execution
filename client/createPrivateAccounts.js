e75f28d0c95d4669f89a6ed2844ade5a3aaffb20
jordan <jordan@bitola.com>
2023-11-18 08:17:31 +0300

    Public keys export

--- client/createPrivateAccounts.js
+++ client/createPrivateAccounts.js
@@ -3,13 +3,23 @@ const fs = require('fs')
 
 const privateKey1 = ethers.randomBytes(32)
 const privateKey1HexString = ethers.hexlify(privateKey1)
-fs.writeFileSync('./keys/account1.private_key', privateKey1HexString)
+fs.writeFileSync('./keys/account1', privateKey1HexString)
 
 const privateKey2 = ethers.randomBytes(32)
 const privateKey2HexString = ethers.hexlify(privateKey2)
-fs.writeFileSync('./keys/account2.private_key', privateKey2HexString)
+fs.writeFileSync('./keys/account2', privateKey2HexString)
 
 // Check the keys
-k1 = fs.readFileSync('./keys/account1.private_key').toString()
-k2 = fs.readFileSync('./keys/account2.private_key').toString()
+k1 = fs.readFileSync('./keys/account1').toString()
+k2 = fs.readFileSync('./keys/account2').toString()
 console.log('Verified:', privateKey1HexString === k1 && privateKey2HexString === k2)
+
+// Get the public key 1 and serialized it to a file
+const signingKey1 = new ethers.SigningKey(privateKey1HexString)
+const publicKey1 = signingKey1.publicKey;
+fs.writeFileSync('./keys/account1.pub', publicKey1)
+
+// Get the public key 2 and serialized it to a file
+const signingKey2 = new ethers.SigningKey(privateKey2HexString)
+const publicKey2 = signingKey2.publicKey;
+fs.writeFileSync('./keys/account2.pub', publicKey2)
